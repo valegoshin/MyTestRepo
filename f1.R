f4636406938c3dc17ba44934e2a0fe821b2af568
valegoshin <valegoshin@gmail.com>
2018-09-20 23:31:39 +0700

    summarise price, carat group_by cut

--- f1.R
+++ f1.R
@@ -3,3 +3,14 @@ library(tidyverse)
 ggplot(diamonds, aes(cut, carat, fill = cut)) + 
   geom_jitter(alpha = .2) + 
   geom_violin(draw_quantiles = .5, color = 'white') 
+
+diamonds %>% 
+  group_by(cut) %>% 
+  summarise(n = n(),
+            me_price = median(price),
+            min_price = min(price),
+            max_price = max(price),
+            me_carat = median(carat),
+            min_carat = min(carat),
+            max_carat = max(carat)) %>% 
+  knitr::kable()
